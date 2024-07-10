8bdf299b011ed1496c1a63d23d286ce91e9d872e
Vlad Prudius <vladvlpr006@gmail.com>
2024-07-11 01:49:58 +0300

    feat: add Expressions nodes

---
+++ AbstractSyntaxTree.h
@@ -0,0 +1,123 @@
+//
+// Created by User on 7/10/2024.
+//
+
+#ifndef ABSTRACTSYNTAXTREE_H
+#define ABSTRACTSYNTAXTREE_H
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+class FunctionDefExpression;
+class Expression;
+using ExpressionPtr = std::shared_ptr<Expression>;
+
+class Expression {
+public:
+    virtual ~Expression() = default;
+    [[nodiscard]] virtual double evaluate() = 0;
+};
+
+class Number final : public Expression {
+    double value;
+public:
+    explicit Number(double value) : value(value) {}
+    double evaluate() override {
+        return value;
+    }
+};
+
+class BinaryExpression final : public Expression {
+    char op;
+    ExpressionPtr left;
+    ExpressionPtr right;
+public:
+    BinaryExpression(char op, ExpressionPtr left, ExpressionPtr right)
+        :op(op), left(std::move(left)), right(std::move(right)) {}
+
+    double evaluate() override {
+        switch (op) {
+            case '+': return left->evaluate() + right->evaluate();
+            case '-': return left->evaluate() - right->evaluate();
+            case '*': return left->evaluate() * right->evaluate();
+            case '/': return left->evaluate() / right->evaluate();
+            default: throw std::runtime_error("Unknown operator");
+        }
+    }
+};
+
+using FunctionDef = std::function<double(const std::vector<double>&)>;
+inline std::map<std::string, FunctionDef> functions;
+inline std::map<std::string, double> variables;
+
+class MethodCallExpression final : public Expression {
+    std::string name;
+    std::vector<ExpressionPtr> args;
+public:
+    MethodCallExpression(std::string name, std::vector<ExpressionPtr> args) : name(std::move(name)), args(std::move(args)) {}
+    double evaluate() override {
+        auto evaluatedArgs = std::vector<double>(args.size());
+        for (size_t i = 0; i < args.size(); ++i) {
+            double value = args[i]->evaluate();
+            evaluatedArgs[i] = value;
+        }
+
+        return functions[name](evaluatedArgs);
+    }
+};
+
+class VariableExpression final : public Expression {
+    std::string name;
+public:
+    explicit VariableExpression(std::string name) : name(std::move(name)) {}
+    double evaluate() override {
+        return variables[name];
+    }
+};
+
+class AssignmentExpression final : public Expression {
+    std::string name;
+    ExpressionPtr value;
+public:
+    AssignmentExpression(std::string name, ExpressionPtr value) : name(std::move(name)), value(std::move(value)) {}
+    double evaluate() override {
+        variables[name] = value->evaluate();
+        return variables[name];
+    }
+};
+
+class StatementListExpression final : public Expression {
+public:
+    std::vector<ExpressionPtr> statements;
+    void addStatement(ExpressionPtr stmt) {
+        statements.push_back(std::move(stmt));
+    }
+    double evaluate() override {
+        double result = 0;
+        for (const auto& stmt : statements) {
+            result = stmt->evaluate();
+        }
+        return result;
+    }
+};
+
+inline std::map<std::string, std::shared_ptr<FunctionDefExpression>> userFunctions;
+
+class FunctionDefExpression final : public Expression {
+public:
+    std::string name;
+    std::vector<std::string> params;
+    ExpressionPtr body;
+
+    FunctionDefExpression(std::string name, std::vector<std::string> params, ExpressionPtr body)
+        : name(std::move(name)), params(std::move(params)), body(std::move(body)) {}
+
+    double evaluate() override { return 0; }
+};
+
+
+#endif //ABSTRACTSYNTAXTREE_H
