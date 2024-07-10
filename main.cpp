cbb78d17c60cf1545f9673394de079d01d9b6995
Vlad Prudius <vladvlpr006@gmail.com>
2024-07-11 02:03:51 +0300

    refactor: add built-in funcs, args i/o

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 #include "AbstractSyntaxTree.h"
 #include "Lexer.h"
@@ -7,26 +8,58 @@
 using namespace std;
 
 int main() {
-    functions["max"] = [](const std::vector<double> &args) {
-        if (args.size() != 2) {
-            throw std::runtime_error("max() function expects 2 arguments");
+    try {
+        // built-ins
+        functions["max"] = [](const std::vector<double> &args) {
+            if (args.size() != 2) {
+                throw std::runtime_error("max() function expects 2 arguments");
+            }
+            return std::max(args[0], args[1]);
+        };
+
+        functions["min"] = [](const std::vector<double> &args) {
+            if (args.size() != 2) {
+                throw std::runtime_error("min() function expects 2 arguments");
+            }
+            return std::min(args[0], args[1]);
+        };
+
+        functions["pow"] = [](const std::vector<double> &args) {
+            if (args.size() != 2) {
+                throw std::runtime_error("pow() function expects 2 arguments");
+            }
+            return std::pow(args[0], args[1]);
+        };
+
+        functions["abs"] = [](const std::vector<double> &args) {
+            if (args.size() != 1) {
+                throw std::runtime_error("abs() function expects 1 argument");
+            }
+            return std::abs(args[0]);
+        };
+
+        std::string input;
+        while (std::getline(std::cin, input)) {
+            Lexer lexer(input);
+            Parser parser(lexer);
+            ExpressionPtr tree = parser.parse();
+
+            double result = tree->evaluate();
+            std::cout << result << std::endl;
         }
-        return std::max(args[0], args[1]);
-    };
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
 
-    functions["min"] = [](const std::vector<double> &args) {
-        if (args.size() != 2) {
-            throw std::runtime_error("min() function expects 2 arguments");
-        }
-        return std::min(args[0], args[1]);
-    };
+    return 0;
+};
 
-    string input = "def myfunc(a, b) { min(a, b) + max(a, b) }; myfunc(3, 4)";
-    // TODO: user defined functions does not work
-    Lexer lexer(input);
-    Parser parser(lexer);
-    ExpressionPtr tree = parser.parse();
 
-    std::cout << "Result: " << tree->evaluate() << std::endl;
-    // std::cout << "a: " << variables["a"] << std::endl;
-}
+// {
+//     // std::cout << "Result: " << tree->evaluate() << std::endl;
+//     // std::cout << "a: " << variables["a"] << std::endl;
+//
+//     // string input = "def myfunc(a, b) { min(a, b) + max(a, b) }; myfunc(3, 4)";
+//     // TODO: user defined functions does not work
+// }
